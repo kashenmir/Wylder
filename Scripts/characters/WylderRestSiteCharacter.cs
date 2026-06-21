feeffa0a7ea7399cbcb8d856e116cfcaa99dcc83
kashenmir <1165511059@qq.com>
2026-06-21 21:31:47 +0800

    fix: 修改bug

--- Scripts/characters/WylderRestSiteCharacter.cs
+++ Scripts/characters/WylderRestSiteCharacter.cs
@@ -2,7 +2,7 @@ using Godot;
 using MegaCrit.Sts2.Core.Nodes.RestSite;
 
 // 这里必须继承 NRestSiteCharacter
-public partial class MyRestSiteCharacter : NRestSiteCharacter
+public partial class WylderRestSiteCharacter : NRestSiteCharacter
 {
 	// 暂时不需要写任何代码，继承父类即可
 }
--- Scripts/characters/WylderSelectionReticle.cs
+++ Scripts/characters/WylderSelectionReticle.cs
@@ -2,7 +2,7 @@ using MegaCrit.Sts2.Core.Nodes.Combat;
 
 namespace wylder.Scripts.characters;
 
-public partial class MySelectionReticle : NSelectionReticle
+public partial class WylderSelectionReticle : NSelectionReticle
 {
 	
 }
