e5051842857efd9ed114408c97b908e961cc09d0
kashenmir <1165511059@qq.com>
2026-07-03 21:25:56 +0800

    fix: bug

--- Scripts/powers/DoubleChasePower.cs
+++ Scripts/powers/DoubleChasePower.cs
@@ -17,7 +17,16 @@ public class DoubleChasePower : ChasePowerModel
         }
         return playCount + Amount;
     }
-    
+
+    public override async Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power,
+        decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        if (power is DoubleChasePower && power.Owner == base.Owner && amount>0 && cardSource !=null)
+        {
+            isInActive = true;
+        }
+    }
+
     public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay)
     {
         if (cardPlay.Card.Owner != Owner.Player || isInActive)
